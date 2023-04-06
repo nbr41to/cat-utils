0b5c3c6696cecb37573a9ab0e0f24ae71fc75a2c
Knob <59198478+nbr41to@users.noreply.github.com>
2023-04-06 22:03:01 +0900

    微修正

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -8,9 +8,9 @@ export const Layout: FC<Props> = ({ children }) => {
   return (
     <div className='max-w-[400px] mx-auto bg-pink-50 h-screen relative'>
       <header className='bg-pink-200'>
-        <h1 className='m-0 text-center'>cat-utils(App名募集中)</h1>
+        <h1 className='m-0 text-center text-2xl'>cat-utils(App名募集中)</h1>
       </header>
-      <main className='pb-4'>{children}</main>
+      <main className='pb-4 my-4 px-4'>{children}</main>
       <footer className='bg-pink-200 absolute bottom-0 w-full'>
         <p className='text-center my-1 text-xs'>© 2023 progLearning</p>
       </footer>
