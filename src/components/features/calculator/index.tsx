279c2deb01636a9e692188909f8f6bda1bdcb25b
Knob <59198478+nbr41to@users.noreply.github.com>
2023-04-06 23:03:40 +0900

    CatCalculatorを分離

---
+++ src/components/features/calculator/index.tsx
@@ -0,0 +1,9 @@
+import CatCalculator from '@/components/features/calculator/CatCalculator';
+
+export default function CalculatorTemplate() {
+  return (
+    <div>
+      <CatCalculator />
+    </div>
+  );
+}
