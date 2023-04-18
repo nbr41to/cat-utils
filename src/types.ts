b8b5b4dd0d8006595f4b6f32b7030ac747503c1f
Knob <59198478+nbr41to@users.noreply.github.com>
2023-04-18 17:22:14 +0900

    修正

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,7 @@ import { signUpSchema } from '@/validations/signUpSchema';
 import { z } from 'zod';
 
 export type SignUpParams = z.infer<typeof signUpSchema>;
-export type LoginParams = Omit<SignUpParams, 'passwordConfirmation'>;
+export type LoginParams = Omit<SignUpParams, 'name' | 'passwordConfirmation'>;
 
 export type User = {
   id: number;
